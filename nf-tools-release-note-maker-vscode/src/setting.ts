d0e0f539b42ba1cfc90a0afede237b782f90f5fa
netpyoung <netpyoung@gmail.com>
2025-11-16 00:01:33 +0900

    change: commandToPreview: 'dotnet release-note preview',

--- nf-tools-release-note-maker-vscode/src/setting.ts
+++ nf-tools-release-note-maker-vscode/src/setting.ts
@@ -2,6 +2,6 @@ import * as settings from './utils/settings';
 
 export const setting = settings.createSettings('nf-tools-release-note-maker-vscode.ReleaseNoteMaker', {
     isEnableFeature: true,
-    commandToPreview: 'dotnet release-note preview --name PREVIEW_NAME --version PREVIEW_VERSION',
+    commandToPreview: 'dotnet release-note preview',
     configFileName: 'ReleaseNote.config.toml',
 });
