cf3c63cb722927a1dfbf56e83e145ea214c31a4f
netpyoung <netpyoung@gmail.com>
2025-11-15 20:52:43 +0900

    update: configuration

--- nf-tools-release-note-maker-vscode/src/setting.ts
+++ nf-tools-release-note-maker-vscode/src/setting.ts
@@ -1,6 +1,6 @@
 import * as settings from './utils/settings';
 
-export const setting = settings.createSettings('nf-tools-release-note-maker-vscode', {
+export const setting = settings.createSettings('nf-tools-release-note-maker-vscode.ReleaseNoteMaker', {
     isEnableFeature: true,
     commandToPreview: 'dotnet release-note preview --name PREVIEW_NAME --version PREVIEW_VERSION',
     configFileName: 'ReleaseNote.config.toml',
